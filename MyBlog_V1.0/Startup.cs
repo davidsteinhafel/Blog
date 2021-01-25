ba512cb32efe4dd03e506900d9fbd5fc3be58a7f
David steinhafel <davidsteinhafel@outlook.com>
2021-01-25 09:58:37 -0600

    first commit test

---
+++ MyBlog_V1.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBlog_V1._0.Startup))]
+namespace MyBlog_V1._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
